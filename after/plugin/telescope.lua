e1db60743ae4b742d4011af468c6a43522a923a6
Vishaal <vishaal.s2020@vitstudent.ac.in>
2024-08-19 22:28:34 +0530

    First Commit

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,11 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n','<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({search = vim.fn.input("Grep > ")})
+end)
+vim.keymap.set('n', '<leader>sn', function()
+        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[S]earch [N]eovim files' })
+
---
+++ lua/vs22202/init.lua
@@ -0,0 +1,10 @@
+require("vs22202.remap")
+require("vs22202.set")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
